1113a23c5ee1f6a7e24031bd8215dc085e33d91d
CameronWafer <129799540+CameronWafer@users.noreply.github.com>
2025-04-27 01:41:37 -0700

    Update UI.py
    added start screen background

--- UI.py
+++ UI.py
@@ -7,13 +7,19 @@ class UI:
         self.window_name = window_name
         cv2.namedWindow(self.window_name)
 
-    def show_start_screen(self):
-        # keeps showing a start screen until the user presses ENTER
+    def show_start_screen(self, bg_path="C:\\Users\\HELIOS-300\\Downloads\\BackgroundRLBL.png"):
+        # show start menu with custom background until enter pressed
+        # load background image if available
+        img = cv2.imread(bg_path)
+        if img is None:
+            background = np.zeros((480, 640, 3), dtype=np.uint8)
+        else:
+            background = cv2.resize(img, (640, 480))
         while True:
-            blank = np.zeros((480, 640, 3), dtype=np.uint8)
+            frame = background.copy()
             # title text
             cv2.putText(
-                blank,
+                frame,
                 "Red Light, Green Light",
                 (80, 200),
                 cv2.FONT_HERSHEY_SIMPLEX,
@@ -24,7 +30,7 @@ class UI:
             )
             # prompt text
             cv2.putText(
-                blank,
+                frame,
                 "press ENTER to start",
                 (170, 300),
                 cv2.FONT_HERSHEY_SIMPLEX,
@@ -35,7 +41,7 @@ class UI:
             )
             # instructions text
             cv2.putText(
-                blank,
+                frame,
                 "q = quit | r = restart",
                 (10, 470),
                 cv2.FONT_HERSHEY_SIMPLEX,
@@ -48,7 +54,7 @@ class UI:
             author = "Author: PandaWafer"
             (tw, _), _ = cv2.getTextSize(author, cv2.FONT_HERSHEY_SIMPLEX, 0.7, 2)
             cv2.putText(
-                blank,
+                frame,
                 author,
                 (640 - tw - 10, 470),
                 cv2.FONT_HERSHEY_SIMPLEX,
@@ -57,10 +63,10 @@ class UI:
                 2,
                 cv2.LINE_AA
             )
-            cv2.imshow(self.window_name, blank)
+            cv2.imshow(self.window_name, frame)
             key = cv2.waitKey(100) & 0xFF
             if key == 13:  # ENTER key
                 break
-        # reset window for the game
+        # reset window for game
         cv2.destroyWindow(self.window_name)
         cv2.namedWindow(self.window_name)
